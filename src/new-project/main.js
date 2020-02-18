90910da66a38597836cc46f6eff75c910a5a701d
Israel-Laguan <israellaguan@gmail.com>
2020-02-18 14:44:35 -0500

    [feat] newProject: form working with validations
    What?:
    - Add validations on HTML and JS
    - Manage errors
    - Move to index when success
    
    Ref. #5
    Breaking Changes?

--- src/new-project/main.js
+++ src/new-project/main.js
@@ -1,6 +1,7 @@
 require('normalize.css/normalize.css')
 require('../css/main.css')
 require('./page.css')
+require('../js/View/newProject');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-contacts')

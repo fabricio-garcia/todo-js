1e88ef6481d45217ce48b215fefa8ec4860353e5
Fabricio Garcia <gael.fabricio@gmail.com>
2020-02-20 11:40:09 -0600

    feat: Link edit function to button in task view
    - Add required event listener to activate edit form in task
    
    Refs. #5

--- src/page-task/main.js
+++ src/page-task/main.js
@@ -6,3 +6,9 @@ require('../js/View/task');
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-contacts');
 });
+
+document.querySelector('.edit-icon').addEventListener('click', evt => {
+  document.querySelector(".edit-task").removeAttribute("hidden");
+  document.querySelector(".task-description").setAttribute("hidden", true);
+  console.log('soething happened!')
+});
